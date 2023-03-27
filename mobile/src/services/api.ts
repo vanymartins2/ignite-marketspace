928c79c0b1f0b28cf075e2e8a5db9ffa5c554293
Vanessa Martins <vanymartins2@gmail.com>
2023-03-27 17:36:31 -0300

    Added .env .env.example files and configuration

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosInstance } from 'axios'
 
+import { BASE_URL, API_PORT } from '@env'
+
 import { AppError } from '@utils/AppError'
 import {
   storageAuthTokenGet,
@@ -26,7 +28,7 @@ type APIInstanceProps = AxiosInstance & {
 }
 
 const api = axios.create({
-  baseURL: 'http://192.168.2.13:3333'
+  baseURL: `${BASE_URL}:${API_PORT}`
 }) as APIInstanceProps
 
 let isRefreshing = false
